435095cf57c35e0939411025d49deee7d123b468
ketanchoyal <sharmaketanr@gmail.com>
2018-12-05 11:13:30 +0530

    Stack implementation

---
+++ Smack-Chat App/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Smack-Chat App
+//
+//  Created by Ketan Choyal on 05/12/18.
+//  Copyright © 2018 Ketan Choyal. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //IBOutlet
+    @IBOutlet weak var menuBtn: UIButton!
+    @IBOutlet weak var chatChannelLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+
+}
+
