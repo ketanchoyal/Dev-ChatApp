a678b994e445c8b63ec9aa91b4206c7f76e2f42d
ketanchoyal <sharmaketanr@gmail.com>
2018-12-08 16:06:57 +0530

    Rounded Button

---
+++ Smack-Chat App/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  Smack-Chat App
+//
+//  Created by Ketan Choyal on 08/12/18.
+//  Copyright © 2018 Ketan Choyal. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius : CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
