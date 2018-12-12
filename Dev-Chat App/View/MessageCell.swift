abb47195931f0b441bd97bbf6611fec72d202c33
ketanchoyal <sharmaketanr@gmail.com>
2018-12-13 00:09:26 +0530

    TimeStamp

--- Dev-Chat App/View/MessageCell.swift
+++ Dev-Chat App/View/MessageCell.swift
@@ -25,6 +25,24 @@ class MessageCell: UITableViewCell {
         messageBodyLbl.text = message.message
         userProfileImg.image = UIImage(named: message.userAvatar)
         userProfileImg.backgroundColor = UserDataService.instance.returnUIColor(component: message.userAvatarColor)
+        
+        //2018-12-10T20:36:14.732Z
+        
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let timeStamp = newFormatter.string(from: finalDate)
+            timeStampLbl.text = timeStamp
+        }
+        
     }
 
 }
